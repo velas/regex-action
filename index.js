93e7e7e5558883befe4fbef45dd84e9a0ac1c45e
olexandr13 <oleksandr.pelykh@gmail.com>
2021-03-30 18:43:51 +0300

    fix param

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ try {
   const regexPattern = core.getInput('regex_pattern');
   const regexFlags = core.getInput('regex_flags');
   const searchString = core.getInput('search_string');
-  // const returnGlobalOrFirst = core.getInput('return_global_or_first_match');
+  const returnGlobalOrFirst = core.getInput('return_global_or_first_match');
   if (!regexPattern) {
     core.setFailed('regex_pattern input is required');
     return;
@@ -21,7 +21,7 @@ try {
     core.setFailed('return_global_or_first_match input is required');
     return;
   }
-  if (returnGlobalOrFirst !== 'global' || returnGlobalOrFirst !== 'first') {
+  if (returnGlobalOrFirst !== 'global' && returnGlobalOrFirst !== 'first') {
     console.log(`return_global_or_first_match: ${return_global_or_first_match}`);
     core.setFailed('return_global_or_first_match param should be "global" or "first"');
     return;
@@ -33,13 +33,13 @@ try {
     return;
   }
   console.log('Found:', matches);
-  // if(returnGlobalOrFirst === 'global') {
+  if(returnGlobalOrFirst === 'global') {
     console.log('set output global "match":', matches);
     core.setOutput('match', matches);
-  // } else {
-    // console.log('set output first "match":', matches);
-    // core.setOutput('match', matches[0]);
-  // }
+  } else {
+    console.log('set output first "match":', matches);
+    core.setOutput('match', matches[0]);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
